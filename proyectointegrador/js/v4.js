f0c1dd0b73ae5fc4f9a30ce13d57975463f6b7a5
Alejandro Balmaceda <servicios@live.com>
2023-02-28 23:45:54 -0300

    280223

---
+++ proyectointegrador/js/v4.js
@@ -0,0 +1,31 @@
+
+document.getElementById("edad").onclick = function (){
+    console.log ("Probamos a ver si sale")
+    document.getElementById('campos').innerHTML = "44 años"
+}
+
+document.getElementById("direccion").onclick = function (){
+    console.log ("Probamos a ver si sale")
+    document.getElementById('campos').innerHTML = "Gral vedia 232"
+}
+document.getElementById("localidad").onclick = function (){
+    console.log ("Probamos a ver si sale")
+    document.getElementById('campos').innerHTML = "Sarandi"
+}
+document.getElementById("partido").onclick = function (){
+    console.log ("Probamos a ver si sale")
+    document.getElementById('campos').innerHTML = "Avellaneda"
+}
+document.getElementById("civil").onclick = function (){
+    console.log ("Probamos a ver si sale")
+    document.getElementById('campos').innerHTML = "Casado, sin hijos"
+}
+document.getElementById("disponibilidad").onclick = function (){
+    console.log ("Probamos a ver si sale")
+    document.getElementById('campos').innerHTML = "Full Time"
+    
+}
+document.getElementById("licencia").onclick = function (){
+    console.log ("Probamos a ver si sale")
+    document.getElementById('campos').innerHTML = "Estandar, Tipo B"
+}
